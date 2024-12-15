c944e408ae9f943be1995eae6c76c9486f65b8d0
Ho3ein <Ho3ein@DESKTOP-08D2HU8>
2024-12-15 12:32:41 +0330

    add Commit func

--- funcs.js
+++ funcs.js
@@ -4,12 +4,16 @@ const user = () => true;
 
 const likepost = () => {
   true;
-}
+};
 
 const disslike = () => {
   false;
-}
+};
 
 const getMe = (user) => {
   return "user";
+};
+
+const SetComment = (comment) => {
+  return comment.body;
 };
